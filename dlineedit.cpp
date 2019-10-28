1e11d3329185a0af760e4b304b3cb0eb7339920d
Chris Moller <moller@mollerware.com>
2019-10-28 14:02:13 -0400

    Lots of stuff

---
+++ dlineedit.cpp
@@ -0,0 +1,6 @@
+#include "dlineedit.h"
+
+DLineEdit::DLineEdit(QWidget *parent) : QWidget(parent)
+{
+
+}
---
+++ dlineedit.h
@@ -0,0 +1,18 @@
+#ifndef DLINEEDIT_H
+#define DLINEEDIT_H
+
+#include <QObject>
+#include <QWidget>
+
+class DLineEdit : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit DLineEdit(QWidget *parent = nullptr);
+
+signals:
+
+public slots:
+};
+
+#endif // DLINEEDIT_H
